9e3a012fd59de5dcc679db47a1920a3e8c275b63
Seox <seanmenglaw2009@gmail.com>
2022-05-27 22:25:20 +0800

    Authentication work-in-progress

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import React from 'react';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React from "react";
 
-import Login from './src/page/login';
-import Lobby from './src/page/lobby';
-import SendBird from 'sendbird';
+import Login from "./src/page/login";
+import Signup from "./src/page/signup";
+import SendBird from "sendbird";
 
-const APP_ID = 'D64A1C3C-01F3-4B09-8173-6556C729FA33';
-const sb = new SendBird({appId: APP_ID});
+const APP_ID = "D64A1C3C-01F3-4B09-8173-6556C729FA33";
+const sb = new SendBird({ appId: APP_ID });
 export const AppContext = React.createContext();
 
 const Stack = createNativeStackNavigator();
@@ -17,13 +17,22 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <AppContext.Provider value={ sb }>
+      <AppContext.Provider value={sb}>
         <Stack.Navigator>
           {/* <View style={styles.container}>
             <Text>Hello World!</Text>
             <StatusBar style="auto" />
           </View> */}
-          <Stack.Screen name="Login" component={Login} options={{ headerTitle: '', headerTransparent: true }} />
+          <Stack.Screen
+            name='Login'
+            component={Login}
+            options={{ headerTitle: "", headerTransparent: true }}
+          />
+          <Stack.Screen
+            name='Signup'
+            component={Signup}
+            options={{ headerTransparent: true }}
+          />
         </Stack.Navigator>
       </AppContext.Provider>
     </NavigationContainer>
@@ -33,16 +42,16 @@ export default function App() {
 const defaultHeaderOptions = {
   headerStyle: {
     height: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
-  headerTintColor: '#fff',
+  headerTintColor: "#fff",
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#2',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#2",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- src/page/login.js
+++ src/page/login.js
@@ -1,80 +1,131 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useEffect, useState, useLayoutEffect } from 'react';
-import { Image, StyleSheet, Text, TouchableOpacity, View, Platform, Button, TextInput, Keyboard } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState, useLayoutEffect } from "react";
+import {
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  TouchableHighlight,
+  View,
+  Platform,
+  Button,
+  TextInput,
+  Keyboard,
+  TouchableWithoutFeedback,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
+import Icon from "react-native-vector-icons/MaterialIcons";
 
-import Icon from 'react-native-vector-icons/MaterialIcons';
+import { AppContext } from "../../App";
+import { auth } from "../../firebase";
 
-import { AppContext } from '../../App';
+export default function Login({ navigation }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [hidden, setHidden] = useState(true);
 
-export default function Lobby() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const handleLogin = () => {
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then((userCredentials) => {
+        const user = userCredentials.user;
+        console.log(user);
+      })
+      .catch((error) => alert(error.message));
+  };
 
   return (
     <AppContext.Consumer>
       {({ sendBird }) => (
-        <>
-          <StatusBar backgroundColor='#AFEEEE' barStyle='light-content' />
-          <SafeAreaView style={{backgroundColor: '#E0FFFF', height:'100%'}}>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          {/* <StatusBar backgroundColor='#AFEEEE' barStyle='light-content' /> */}
+          <SafeAreaView style={{ ...styles.screen }}>
             <View style={styles.container}>
-              <Image source={require('../../assets/chatee-logo.png')} />
+              <Image source={require("../../assets/chatee-logo.png")} />
               <TextInput
-                style={{...styles.input, marginTop: '10%'}}
+                style={{ ...styles.input, marginTop: "10%" }}
                 placeholder='Username'
                 onChangeText={setUsername}
               />
-              <TextInput
-                style={styles.input}
-                placeholder='Password'
-                onChangeText={setPassword}
-              />
+              <View
+                style={{
+                  ...styles.input,
+                  flexDirection: "row",
+                  alignItems: "center",
+                }}
+              >
+                <TextInput
+                  placeholder='Password'
+                  style={{ width: "100%" }}
+                  maxLength={30}
+                  secureTextEntry={hidden}
+                  keyboardType={hidden ? "default" : "visible-password"}
+                  onChangeText={setPassword}
+                />
+                <TouchableHighlight style={styles.icon} underlayColor={"blue"}>
+                  <Icon
+                    style={{ fontSize: 25 }}
+                    name={hidden ? "visibility-off" : "visibility"}
+                    onPress={() => setHidden(!hidden)}
+                  />
+                </TouchableHighlight>
+              </View>
               <TouchableOpacity
                 style={styles.button}
-                onPress={() => console.log(username + '\n' + password)}
+                onPress={() => console.log(username + "\n" + password)}
                 color='#841584'
               >
-                <Text style={{color: 'white'}}>Log In</Text>
+                <Text style={{ color: "white" }}>Log In</Text>
               </TouchableOpacity>
+              <Text>OR</Text>
               <TouchableOpacity
                 style={styles.button}
-                onPress={() => 1+1}
+                onPress={() =>
+                  navigation.navigate("Signup", { user: username })
+                }
                 color='#841584'
               >
-                <Text style={{color: 'white'}}>Sign Up</Text>
+                <Text style={{ color: "white" }}>Sign Up</Text>
               </TouchableOpacity>
             </View>
           </SafeAreaView>
-        </>
+        </TouchableWithoutFeedback>
       )}
     </AppContext.Consumer>
   );
 }
 
 const styles = StyleSheet.create({
+  screen: {
+    backgroundColor: "#E0FFFF",
+    height: "100%",
+  },
   container: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingTop: '10%',
+    justifyContent: "center",
+    alignItems: "center",
+    paddingTop: "10%",
   },
   input: {
-    borderColor: 'black',
-    borderStyle: 'solid',
+    borderColor: "gray",
+    borderStyle: "solid",
     borderWidth: 2,
     borderRadius: 10,
-    padding: '2%',
-    width: '80%',
-    textAlign: 'center',
-    margin: '1%',
+    padding: "2%",
+    width: "80%",
+    margin: "1%",
   },
   button: {
-    backgroundColor: 'green',
+    backgroundColor: "green",
     height: 40,
     width: 100,
-    alignItems: 'center',
-    borderRadius:10,
+    alignItems: "center",
+    borderRadius: 10,
     padding: 10,
     margin: 3,
+  },
+  icon: {
+    position: "absolute",
+    right: 8,
   },
 });
